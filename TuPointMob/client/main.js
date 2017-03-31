cd1ebc365e48a9be6726e5c28cc41696b2863f6c
mediaganathe <mediaganathe@gmail.com>
2017-03-31 21:08:37 +0530

    first demo

--- TuPointMob/client/main.js
+++ TuPointMob/client/main.js
@@ -1,12 +1,18 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import 'bootstrap';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+/*Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
-  this.counter = new ReactiveVar(0);
+  //this.counter = new ReactiveVar(0);
+
+  $('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+});*/
 
 Template.hello.helpers({
   counter() {
@@ -20,3 +26,8 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
+
+ 
+
+
